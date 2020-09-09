f46cfd8dff6e71a9252c2fb7f324335e71c47d16
phillipmblaine <66438993+phillipmblaine@users.noreply.github.com>
2020-09-09 12:28:45 -0400

    Revert "Phillip"

--- react-fundamentals/src/App.js
+++ react-fundamentals/src/App.js
@@ -1,34 +1,17 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import Nasa from "./components/nasa/Nasa";
 import OpenWeather from "./components/openweather/Openweather";
-import Zomato from "./components/zomato/ZomatoApp";
-
+import Zomato from "./components/zomato/Zomato";
 
 function App() {
-
-	const [lat, setLat] = useState(0);
-	const [lon, setLon] = useState(0);
-	useEffect(() => {
-		if (navigator.geolocation) {
-			navigator.geolocation.getCurrentPosition(getCoord);
-		} else {
-			alert("Geolocation is not supported by this browser.");
-		}
-	}, ['']);
-
-	const getCoord = (position) => {
-		setLat(position.coords.latitude);
-		setLon(position.coords.longitude);
-	};
-
 	return (
 		<div className="App">
 			PAGE HEADER
 			<Nasa />
-			<OpenWeather lat={lat} lon={lon} />
-			<Zomato lat={lat} lon={lon} />
+			<OpenWeather />
+			<Zomato />
 		</div>
 	);
 }
