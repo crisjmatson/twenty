7c378c3ff9e5dd77f88177d983d96bc0386b67a9
Phillip M. Blaine <phillipmblaine@gmail.com>
2020-09-09 08:40:00 -0400

    phillip branch commit

--- react-fundamentals/src/App.js
+++ react-fundamentals/src/App.js
@@ -1,19 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import Nasa from "./components/nasa/Nasa";
 import OpenWeather from "./components/openweather/Openweather";
-import Zomato from "./components/zomato/Zomato";
-
-https://github.com/crisjmatson/24HourProject
+import Zomato from "./components/zomato/ZomatoApp";
 
 function App() {
+
+	const [lat, setLat] = useState(0);
+	const [lon, setLon] = useState(0);
+	useEffect(() => {
+		if (navigator.geolocation) {
+			navigator.geolocation.getCurrentPosition(getCoord);
+		} else {
+			alert("Geolocation is not supported by this browser.");
+		}
+	}, ['']);
+
+	const getCoord = (position) => {
+		setLat(position.coords.latitude);
+		setLon(position.coords.longitude);
+	};
+
 	return (
 		<div className="App">
 			PAGE HEADER
 			<Nasa />
-			<OpenWeather />
-			<Zomato />
+			<OpenWeather lat={lat} lon={lon} />
+			<Zomato lat={lat} lon={lon} />
 		</div>
 	);
 }
--- react-fundamentals/src/components/zomato/Zomato.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const Zomato = () => {
-	return <div>ZOMATO</div>;
-};
-
-export default Zomato;
