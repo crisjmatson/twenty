68ad2964737400666b315e1ea50c6815bdb071b2
Cris Matson <crisjmatson@gmail.com>
2020-09-09 11:48:03 -0400

    mvp

--- react-fundamentals/src/App.js
+++ react-fundamentals/src/App.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
+import { Jumbotron, Container } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.css";
 import "./App.css";
 import Nasa from "./components/nasa/Nasa";
 import OpenWeather from "./components/openweather/Openweather";
 import Zomato from "./components/zomato/Zomato";
-import WeatherResult from "./components/openweather/WeatherResult";
 
 function App() {
 	const [lat, setLat] = useState(0);
@@ -15,22 +15,23 @@ function App() {
 		} else {
 			alert("Geolocation is not supported by this browser.");
 		}
-	}, [""]);
+	});
+
 	const getCoord = (position) => {
 		setLat(position.coords.latitude);
 		setLon(position.coords.longitude);
 	};
 
 	return (
-		<div className="App">
-			PAGE HEADER
-			<div>
-				{lat}, {lon}
-			</div>
-			<hr />
+		<div>
+			<Jumbotron fluid className="mainJumbo">
+				<Container fluid>
+					<h1>you are here.</h1>
+				</Container>
+			</Jumbotron>
 			<Nasa />
 			<OpenWeather lat={lat} lon={lon} />
-			<Zomato lat={lat} lon={lon} />
+			<Zomato />
 		</div>
 	);
 }
--- react-fundamentals/src/components/openweather/Openweather.js
+++ react-fundamentals/src/components/openweather/Openweather.js
@@ -1,10 +1,21 @@
 import React, { useState } from "react";
-import WeatherResult from "./WeatherResult";
+import "./Openweather.css";
+import {
+	Jumbotron,
+	Container,
+	Button,
+	ButtonDropdown,
+	DropdownToggle,
+	Card,
+} from "reactstrap";
 const base = "http://api.openweathermap.org/data/2.5/weather";
 const key = "fa3d2da352ac2f3a043c92d63aff4a5b";
 
 const OpenWeather = (props) => {
 	const [weather, setWeather] = useState();
+	const [showResults, setShowResults] = useState(false);
+	const [dropdownClosed, setDropDown] = useState(false);
+	const [unit, setUnit] = useState(true);
 	const longitude = props.lon;
 	const latitude = props.lat;
 
@@ -15,22 +26,103 @@ const OpenWeather = (props) => {
 		let url = `${base}?lat=${latitude}&lon=${longitude}&appid=${key}`;
 		fetch(url)
 			.then((res) => {
-				console.log(res, url);
+				//console.log(res, url);
 				return res.json();
 			})
 			.then((resolve) => {
-				console.log(resolve);
+				//console.log(resolve);
 				//setWeather(JSON.stringify(resolve));
 				setWeather(resolve);
+				console.log(weather);
+				displayAll(weather);
 			})
 			.catch((err) => console.log(err));
 	};
 
+	const toggle = (e) => {
+		e.preventDefault();
+		setDropDown(!dropdownClosed);
+	};
+
+	const switchUnit = (e) => {
+		e.preventDefault();
+		setUnit(!unit);
+	};
+	const kToC = (num) => num - 273.15;
+	const kToF = (num) => (num - 273.15) * 1.8 + 32;
+
+	const displayAll = (weather) => {
+		setShowResults(!showResults);
+	};
+
 	return (
-		<div>
-			<button onClick={(e) => fetcher(e)}>Show</button>
-			<WeatherResult weather={weather} />
-		</div>
+		<Jumbotron fluid className="openBackWeather">
+			<Container fluid>
+				{showResults ? (
+					<div>
+						<h1 className="display-3 whiteTextWeather shadowWeather">
+							{weather.name}
+
+							{unit ? (
+								<div className="whiteTextWeather">
+									{kToF(weather.main.temp).toFixed(2)}&#176;
+									<h6>farenheit</h6>
+								</div>
+							) : (
+								<div className="whiteTextWeather">
+									{kToC(weather.main.temp).toFixed(2)}&#176;
+									<h6>centigrade</h6>
+								</div>
+							)}
+						</h1>
+						<h6 className="lead whiteTextWeather">
+							{weather.weather[0].main}, {weather.weather[0].description}
+						</h6>
+
+						<ButtonDropdown>
+							<Button
+								onClick={(e) => switchUnit(e)}
+								id="caret"
+								color="secondary"
+							>
+								Switch
+							</Button>
+
+							<DropdownToggle
+								caret
+								color="secondary"
+								onClick={(e) => toggle(e)}
+							/>
+						</ButtonDropdown>
+						{dropdownClosed ? (
+							<div>
+								<br />
+								<div>
+									<ul class="list-group list-group-flush">
+										<li class="list-group-item">
+											pressure: {weather.main.pressure}
+										</li>
+										<li class="list-group-item">
+											humidity: {weather.main.humidity}%
+										</li>
+										<li class="list-group-item">
+											visibility: {weather.visibility}
+										</li>
+										<li class="list-group-item">
+											wind speed: {weather.wind.speed}
+										</li>
+									</ul>
+								</div>
+							</div>
+						) : (
+							<div></div>
+						)}
+					</div>
+				) : (
+					<Button onClick={(e) => fetcher(e)}>current weather..</Button>
+				)}
+			</Container>
+		</Jumbotron>
 	);
 };
 
